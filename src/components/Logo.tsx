9aebbfdee2bd57e220d9361b262aef8c68971dc0
Son Nguyen <sonhoang95@gmail.com>
2022-07-18 18:31:13 -0500

    complete error page, file structures set up

---
+++ src/components/Logo.tsx
@@ -0,0 +1,10 @@
+import logo from "../assets/images/logo.svg";
+
+const Logo = () => {
+  return (
+    <div>
+      <img src={logo} alt="jobster logo" className="logo" />
+    </div>
+  );
+};
+export default Logo;
